b0417960ee5b2cedad4e8182e55b486f4f3be98f
Aurimas Pokvytis <aurimas.pokvytis@metasite.net>
2017-06-02 17:25:26 +0300

    PageObject file updated

--- e2e/pageObject.ts
+++ e2e/pageObject.ts
@@ -1,3 +1,9 @@
+import {element, by } from 'protractor';
+
 export class PageObject{
-    
+    name = element(by.css("#id"));
+
+    getName: any = () => {
+        return this.name.getText();
+    }
 }
