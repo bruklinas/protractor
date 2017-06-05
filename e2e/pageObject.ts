29df17661b878ef8b36307cea467e28607a627c7
bruklinas <bruklinas@gmail.com>
2017-06-05 10:16:44 +0300

    Remote master updated.

--- e2e/pageObject.ts
+++ e2e/pageObject.ts
@@ -18,4 +18,8 @@ export class PageObject {
     thirdFunction() {
         console.log("Third function from branch 'master' executed");
     }
+    
+    remoteMasterFunction() : number{
+        return 3;
+    }
 }
